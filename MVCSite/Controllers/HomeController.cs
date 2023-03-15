541346afabbb5046853fab5b27c12d628f08ae8a
Thoks10a <nicholas.isola@hotmail.com>
2023-03-15 16:06:49 -0300

    Add project files.

---
+++ MVCSite/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCSite.Models;
+using System.Diagnostics;
+
+namespace MVCSite.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index() // Startup inicia o Home e o Index (Pagina principal da home)
+        {
+            return View();
+        }
+
+        public IActionResult Privacy() // Pagina de privacidade
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error() // Pagina de erro
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
